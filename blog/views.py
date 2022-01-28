816ae35dd757b3dac360b0449c0de87e54bb990b
SaberHardy <samirsamado5@gmail.com>
2022-01-28 19:46:46 +0300

    add forms for creating posts

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from blog.models import Post
+from .forms import PostModelForm
 
 
 def home(request):
     posts = Post.objects.all()
-
+    if request.method == "POST":
+        form = PostModelForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('home')
+    else:
+        form = PostModelForm()
     context = {
-        'posts': posts
+        'posts': posts,
+        'form': form
     }
-    return render(request, 'blog/index.html', context)
 
-
+    return render(request, 'blog/index.html', context=context)
